ecd3d3d3105d6e14d0cf177b22855ef4013ff220
ing-arriola <ingeniero.arriola@gmail.com>
2020-04-12 11:48:31 -0600

    Added flip card and checkForMatch functions

--- memoryGame/memory.js
+++ memoryGame/memory.js
@@ -52,17 +52,61 @@ document.addEventListener('DOMContentLoaded', ()=>{
      ]
 
      const grid = document.querySelector('.grid')
+     const resultDisplay = document.getElementById('result')
+     let cardsChosen = []
+     let cardsChosenId = []
+     let cardsWon = []
+
+     const checkForMatch = () =>{
+         let cards = document.querySelectorAll('img')
+         const optionOneId = cardsChosen[0]
+         const optionTwoId = cardsChosen[1]
+
+         if (optionOneId === optionTwoId) {
+             alert('You found a match :-)')
+             cards[optionOneId].setAttribute('src','images/white.png')
+             cards[optionTwoId].setAttribute('src','images/white.png')
+             cardsWon.push(cardsChosen)
+         }else{
+            cards[optionOneId].setAttribute('src','images/blank.png')
+            cards[optionTwoId].setAttribute('src','images/blank.png')
+            alert('Sorry for you :-( Try again')
+         }
+         cardsChosen = []
+         cardsChosenId = []
+         resultDisplay
+
+     }
+
+     const flipCard = () =>{
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src',cardArray[cardId].img)//set the image from the array
+        //check for a match, but only if the user has been selected two cards
+        if (cardsChosen.length === 2) {
+            setTimeout(() => {
+                checkForMatch()
+            }, 500);
+        }
+
+     }
 
      //Creating board
-
-     for (let i = 0; i < cardArray.length; i++) {
-         let card = document.createElement('img')
-         card.setAttribute('src','images/blank.png')
-         card.setAttribute('data-id',i)
-         card.setAttribute('class','element')
-         //card.addEventListener('click',flip)
-         grid.appendChild(card)
-         
+     const createBoard = ()=>{
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src','images/blank.png')
+            card.setAttribute('data-id',i)
+            card.setAttribute('class','element')
+            card.addEventListener('click',flipCard)
+            grid.appendChild(card)
+            
+        }
      }
+     createBoard()
+
+     
+     
 
 })
