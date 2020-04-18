a96fcdd58fcadb85798d6d147e0570527cf7fb04
ing-arriola <ingeniero.arriola@gmail.com>
2020-04-18 16:28:22 -0600

    Some changes to restart the game and to stop it well

--- whackAMole/mole.js
+++ whackAMole/mole.js
@@ -1,29 +1,31 @@
 const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
 const timeleft = document.getElementById('seconds-left')
+const starButton = document.getElementById('play')
 let score = document.getElementById('score')
 
-let result = 0
+let result
 let hitPosition
-let currentTime = timeleft.textContent
+let currentTime
 let timerId
+let timer
+let again
 const reStartGame = () => {
     result=0
     score.textContent=result
-    currentTime=60
+    currentTime=10
     timeleft.textContent = currentTime
     setCountDown()
 }
-
 //Showing the mole in a random square after the last time that appear
 const randomSquare = () =>{
-    square.forEach(className => {
-        className.classList.remove('mole')
-    })
-    let randomPosition = square[Math.floor(Math.random()*9)]
-    randomPosition.classList.add('mole')
-    //assign the id of the randomPosition to hitPosition for us to use later
-    hitPosition = randomPosition.id
+        square.forEach(className => {
+            className.classList.remove('mole')
+        })
+        let randomPosition = square[Math.floor(Math.random()*9)]
+        randomPosition.classList.add('mole')
+        //assign the id of the randomPosition to hitPosition for us to use later
+        hitPosition = randomPosition.id
 }
 //Setting each square with an eventListener that wil be triggered 
 //when the mouseup event happen
@@ -40,33 +42,34 @@ const setSquares = () => {
 
 
 const moveMole = () => {
-    let timerId = null
-    timerId = setInterval(() => {
-        randomSquare()
-    }, 800);
+    timerId = setInterval(randomSquare, 800);
 }
 
 const setCountDown = () => {
-    timerId  = setInterval(() => {
-        countDown()  
-    }, 1000);
+    timer  = setInterval(countDown, 1000);
 }
 
 const countDown = () => {
     currentTime--
     timeleft.textContent = currentTime
     //After 1 minute, the game is over =O
-    console.log(currentTime)
     if (currentTime === 0) {
-        clearInterval(timerId)
+        //clearInterval(timerId)
         alert(`Game over, your score is: ${result}`)
-        reStartGame()
+        clearInterval(timerId)
+        clearInterval(timer)
+        score.textContent=0
+        }
     }
-}
 
+    setSquares()
 
+starButton.addEventListener('click',()=>{
+    result=0
+    currentTime=10
+    score.textContent=result
+    timeleft.textContent = currentTime
+    moveMole()
+    setCountDown()
+})
 
-
-setSquares()
-moveMole()
-setCountDown()
